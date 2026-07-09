882883bfa3663dd22a2866d7123b7b694857ecd6
jeiare01-lab <jeiare01@gmail.com>
2026-07-09 16:38:05 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HCDCareerLattice from './components/HCDCareerLattice'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HCDCareerLattice />
+  </React.StrictMode>,
+)
