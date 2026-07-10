3dfda962d5b9ae35e86f3507b50df264d8945f4d
jeiare01-lab <jeiare01@gmail.com>
2026-07-10 13:33:45 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    port: 3000,
+    strictPort: false,
+  }
 })
